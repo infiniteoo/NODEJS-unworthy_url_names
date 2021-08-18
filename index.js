02b0ada063da6b7474dc5450e8cbb29504b61f95
Troy Dorman <troydorman@gmail.com>
2021-08-17 23:17:34 -0600

    iterates over banned keywords array

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const isPorn = require("is-porn");
 const psl = require("psl");
 const extractHostname = require("./extractHostname");
+const bannedKeywords = require("./bannedKeywords");
 
 // let url = "https://www.pornhub.com/view_video.php?viewkey=ph5b576748add25";
 let url = "https://www.churchofsatan.com/";
@@ -12,12 +13,12 @@ isPorn(psl.get(extractHostname(url)), (err, status) => {
   } else {
     // if returns false, then search extractedHostname for keywords
     console.log("this is not a porn site");
-    if (psl.get(extractHostname(url)).includes("satan")) {
-      console.log(
-        "contains banned keyword. this is an unworthy link for our site"
-      );
-    }
+    bannedKeywords.forEach((keyword) => {
+      if (psl.get(extractHostname(url)).includes(keyword)) {
+        console.log(
+          `contains banned keyword: ${keyword}. this is an unworthy link for our site`
+        );
+      }
+    });
   }
 });
-
-
